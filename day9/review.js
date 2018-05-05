8f25842832d024e43653aa6e062ac3d39b09a72e
Jacob Tran <jacobtran@gmail.com>
2018-05-05 13:10:51 -0700

    add day 9 files

---
+++ day9/review.js
@@ -0,0 +1,70 @@
+// good morning everyone!
+// day 1, 2
+// js - the only native programming language in the browswer
+// brendan eich - purist
+let name = "Jacob";
+// creating a variable, and storing a string in memory
+// memory - ram or solid state
+
+// day 3, 4
+// structures of data
+//                      0          1            2
+let packingList = ["sunscreen", "shorts", "sunglasses"];
+// array - an indexed list of information
+// city block
+packingList[2];
+
+// objects
+// object oriented based?
+const packingList = {
+  0: "sunscreen",
+  1: "shorts",
+  2: "sunglasses"
+};
+
+packingList[0];
+
+const car = {
+  colour: "red",
+  make: "tesla",
+  model: "model 3"
+};
+
+car.colour;
+car.make;
+car.model;
+
+// day 5, 6
+// JSON - JavaScript Object Notation
+let teachers = [{name: "Jacob"}, {name: "Tam"}, {name: "Jason"}];
+
+const dog = {
+  tricks: ["sit", "beg", "roll over"],
+  litter: [{name: "Fido"}, {name: "Spot"}, {name: "Summer"}]
+};
+
+// day 7, 8
+// asychronous programming
+// event loop - order wheel at a restaurant
+setTimeout(fn, delay);
+
+setTimeout(function(){
+  console.log("dessert is ready")
+}, 5000);
+
+setTimeout(function(){
+  console.log("steak is ready")
+}, 3000);
+
+// event based programming
+
+
+
+
+
+
+
+
+
+
+//
