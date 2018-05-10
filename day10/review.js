d18347aa6ee1cb55d643f02574132cde733cba68
Jacob Tran <jacobtran@gmail.com>
2018-05-10 09:15:42 -0700

    add day 10 files

---
+++ day10/review.js
@@ -0,0 +1,8 @@
+// chat app
+
+// 1. form
+// 2. log, conversation
+// 2a. name: sentence
+// 3. UX - clear the text field after the user submitted their sentence
+// 3a. give the text field focus so we can just start typing
+// 4. admin: delete or remove
